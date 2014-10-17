c2619d2e99e637b297618e4afcb3a2242ac7b2ec
pirminis <pirminis@gmail.com>
2014-10-18 02:20:00 +0300

    Added tests.

--- tests/MaybeTest.php
+++ tests/MaybeTest.php
@@ -148,6 +148,118 @@ class MaybeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame($expectedValue, $maybeArray['person']['name']->val());
     }
+
+    public function testSome()
+    {
+        $expectedValue = true;
+        $data = '';
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->some());
+
+        $expectedValue = true;
+        $data = 'hello world!';
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->some());
+
+        $expectedValue = true;
+        $data = false;
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->some());
+
+        $expectedValue = false;
+        $data = null;
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->some());
+    }
+
+    public function testNone()
+    {
+        $expectedValue = true;
+        $data = '';
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->none());
+
+        $expectedValue = false;
+        $data = 'hello world!';
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->none());
+
+        $expectedValue = true;
+        $data = false;
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->none());
+
+        $expectedValue = true;
+        $data = null;
+        $value = new Maybe($data);
+
+        $this->assertSame($expectedValue, $value->none());
+    }
+
+    public function testSimpleMap()
+    {
+        $age = new Maybe(28);
+        $modifiedAge = $age->map(function($num) {
+            return new Maybe($num->val() * 10);
+        });
+        $expectedValue = 280;
+
+        $this->assertSame($expectedValue, $modifiedAge->val());
+    }
+
+    public function testArrayMap()
+    {
+        $ages = new Maybe([12, 28, 68]);
+        $modifiedAge = $ages->map(function($num) {
+            return new Maybe($num->val() / 4.0);
+        });
+        $expectedValues = [3.0, 7.0, 17.0];
+
+        $this->assertSame($expectedValues[0], $modifiedAge[0]->val());
+        $this->assertSame($expectedValues[1], $modifiedAge[1]->val());
+        $this->assertSame($expectedValues[2], $modifiedAge[2]->val());
+    }
+
+    public function testNestedObjectsAndNestedMaps()
+    {
+        $expectedValue = 'John';
+        $expectedValue2 = 'Peter';
+        $order = new Maybe(new Order(new User($expectedValue)));
+
+        $name = $order->map(function($order) {
+            return $order->getUser()->map(function($user) {
+                return $user->getName()->map(function($name) {
+                    return $name;
+                });
+            });
+        });
+        $name2 = $order->getUser()->getName()->map(function($name) use($expectedValue2) {
+            return new Maybe($expectedValue2);
+        });
+
+        $this->assertSame($expectedValue, $name->val());
+        $this->assertSame($expectedValue2, $name2->val());
+    }
+
+    public function testMapOnNullMonad()
+    {
+        $expectedValue = 'some';
+        $expectedValue2 = 'none';
+
+        $null = new Maybe(null);
+        $modifiedNull = $null->map(function($value) use ($expectedValue, $expectedValue2) {
+            return new Maybe($value->some() ? $expectedValue : $expectedValue2);
+        });
+
+        $this->assertSame($expectedValue2, $modifiedNull->val());
+    }
 }
 
 class User
